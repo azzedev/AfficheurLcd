2016500a1e258193992debd8e27e1d3eb4cfa20f
Azzedev <azzedine.dev@outlook.Fr>
2024-03-21 06:01:32 +0100

    Test vert pour 10, TODO: Test rouge pour d'autre nombre a 2 chiffres

--- AfficheurLcd/ConvertisseurLcd.cs
+++ AfficheurLcd/ConvertisseurLcd.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic;
 
 namespace AfficheurLcd
 {
@@ -15,19 +16,36 @@ namespace AfficheurLcd
 				Exeptions.ThrowIfNegative(nombre);
 			}
 
-			return nombre switch
+			if (nombre >= 0 && nombre <= 9)
 			{
-				0 => Nombres.Zero,
-				1 => Nombres.Un,
-				2 => Nombres.Deux,
-				3 => Nombres.Trois,
-				4 => Nombres.Quatre,
-				5 => Nombres.Cinq,
-				6 => Nombres.Six,
-				7 => Nombres.Sept,
-				8 => Nombres.Huit,
-				9 => Nombres.Neuf,
-			};
+				return nombre switch
+				{
+					0 => Nombres.Zero,
+					1 => Nombres.Un,
+					2 => Nombres.Deux,
+					3 => Nombres.Trois,
+					4 => Nombres.Quatre,
+					5 => Nombres.Cinq,
+					6 => Nombres.Six,
+					7 => Nombres.Sept,
+					8 => Nombres.Huit,
+					9 => Nombres.Neuf,
+				};
+			}
+
+			else if (nombre == 10)
+			{
+				var un = Nombres.Un.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+				var zero = Nombres.Zero.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+				var ligne1 = un[0] + " " + zero[0];
+				var ligne2 = un[1] + " " + zero[1];
+				var ligne3 = un[2] + " " + zero[2];
+
+				return $"{ligne1}{Environment.NewLine}{ligne2}{Environment.NewLine}{ligne3}";
+			}
+
+			return "";
 		}
 
 	}
