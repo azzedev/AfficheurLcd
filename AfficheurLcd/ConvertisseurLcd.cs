6dc092ea7d73026e907acd3c9380a11de91ba72b
Azzedev <azzedine.dev@outlook.Fr>
2024-03-21 05:14:29 +0100

    Test vert pour l'exeption et code refactorisé pour les exeptions dans une classe todo: test pour les entrée null ?

--- AfficheurLcd/ConvertisseurLcd.cs
+++ AfficheurLcd/ConvertisseurLcd.cs
@@ -12,25 +12,23 @@ namespace AfficheurLcd
 		{
 			if (nombre < 0)
 			{
-				throw new ArgumentException("Veuillez entrer un nombre positif.");
+				Exeptions.ThrowIfNegative(nombre);
 			}
-			else
+
+			return nombre switch
 			{
-				return nombre switch
-				{
-					0 => Nombres.Zero,
-					1 => Nombres.Un,
-					2 => Nombres.Deux,
-					3 => Nombres.Trois,
-					4 => Nombres.Quatre,
-					5 => Nombres.Cinq,
-					6 => Nombres.Six,
-					7 => Nombres.Sept,
-					8 => Nombres.Huit,
-					9 => Nombres.Neuf,
-				};
-			}
-			
+				0 => Nombres.Zero,
+				1 => Nombres.Un,
+				2 => Nombres.Deux,
+				3 => Nombres.Trois,
+				4 => Nombres.Quatre,
+				5 => Nombres.Cinq,
+				6 => Nombres.Six,
+				7 => Nombres.Sept,
+				8 => Nombres.Huit,
+				9 => Nombres.Neuf,
+			};
 		}
+
 	}
 }
---
+++ AfficheurLcd/Exeptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AfficheurLcd
+{
+	public static class Exeptions
+	{
+		public static void ThrowIfNegative(int number, string message = null)
+		{
+			if (number < 0)
+			{
+				throw new ArgumentException("Veuillez entrer un nombre positif.");
+			}
+		}
+	}
+}
