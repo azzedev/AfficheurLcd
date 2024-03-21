f8034b044dbd53216cf100bfde9f1a04abaa7d17
Azzedev <azzedine.dev@outlook.Fr>
2024-03-21 04:34:30 +0100

    Code Refactorisé TODO => méthode pour prendre un entier en paramètre et retourner l''affichage en LCD

--- AfficheurLcd.tests/AfficheurLcdTests.cs
+++ AfficheurLcd.tests/AfficheurLcdTests.cs
@@ -2,112 +2,33 @@ namespace AfficheurLcd.tests
 {
 	public class AfficheurLcdTests
 	{
-		[Fact]
-		public void AfficherNombre1()
+		[Theory]
+		[InlineData(0, " _ " + "\n" + "| |" + "\n" + "|_|")]
+		[InlineData(1, "   " + "\n" + "  |" + "\n" + "  |")]
+		[InlineData(2, " _ " + "\n" + " _|" + "\n" + "|_ ")]
+		[InlineData(3, " _ " + "\n" + " _|" + "\n" + " _|")]
+		[InlineData(4, "   " + "\n" + "|_|" + "\n" + "  |")]
+		[InlineData(5, " _ " + "\n" + "|_ " + "\n" + " _|")]
+		[InlineData(6, " _ " + "\n" + "|_ " + "\n" + "|_|")]
+		[InlineData(7, " _ " + "\n" + "  |" + "\n" + "  |")]
+		[InlineData(8, " _ " + "\n" + "|_|" + "\n" + "|_|")]
+		[InlineData(9, " _ " + "\n" + "|_|" + "\n" + " _|")]
+		public void AfficherNombre(int nombre, string expected)
 		{
-			var expected = "   " + Environment.NewLine +
-			                     "  |" + Environment.NewLine +
-			                     "  |";
-
-			var result = Nombres.Un;
-
-			Assert.Equal(expected, result);
-		}
-
-		[Fact]
-		public void AfficherNombre2()
-		{
-			var expected = " _ " + Environment.NewLine + 
-			                     " _|" + Environment.NewLine +
-			                     "|_ ";
-			var result = Nombres.Deux;
-
-			Assert.Equal(expected, result);
-		}
-		[Fact]
-		public void AfficherNombre3()
-		{
-			var expected = " _ " + Environment.NewLine +
-			                     " _|" + Environment.NewLine +
-								 " _|";
-			var result = Nombres.Trois;
-
-			Assert.Equal(expected, result);
-		}
-
-		[Fact]
-		public void AfficherNombre4()
-		{
-			var expected = "   " + Environment.NewLine +
-						         "|_|" + Environment.NewLine +
-						         "  |";
-			var result = Nombres.Quatre;
-
-			Assert.Equal(expected, result);
-		}
-
-		[Fact]
-		public void AfficherNombre5()
-		{
-			var expected = " _ " + Environment.NewLine +
-			                     "|_ " + Environment.NewLine +
-			                     " _|";
-			var result = Nombres.Cinq;
-
-			Assert.Equal(expected, result);
-		}
-
-		[Fact]
-		public void AfficherNombre6()
-		{
-			var expected = " _ " + Environment.NewLine +
-			                     "|_ " + Environment.NewLine +
-						         "|_|";
-			var result = Nombres.Six;
-
-			Assert.Equal(expected, result);
-		}
-
-		[Fact]
-		public void AfficherNombre7()
-		{
-			var expected = " _ " + Environment.NewLine +
-						         "  |" + Environment.NewLine +
-						         "  |";
-			var result = Nombres.Sept;
-
-			Assert.Equal(expected, result);
-		}
-
-		[Fact]
-		public void AfficherNombre8()
-		{
-			var expected = " _ " + Environment.NewLine +
-						         "|_|" + Environment.NewLine +
-						         "|_|";
-			var result = Nombres.Huit;
-
-			Assert.Equal(expected, result);
-		}
-
-		[Fact]
-		public void AfficherNombre9()
-		{
-			var expected = " _ " + Environment.NewLine +
-						         "|_|" + Environment.NewLine +
-						         " _|";
-			var result = Nombres.Neuf;
-
-			Assert.Equal(expected, result);
-		}
-
-		[Fact]
-		public void AfficherNombre0()
-		{
-			var expected = " _ " + Environment.NewLine +
-						         "| |" + Environment.NewLine +
-						         "|_|";
-			var result = Nombres.Zero;
+			expected = expected.Replace("\n", Environment.NewLine);
+			string result = nombre switch
+			{
+				0 => Nombres.Zero,
+				1 => Nombres.Un,
+				2 => Nombres.Deux,
+				3 => Nombres.Trois,
+				4 => Nombres.Quatre,
+				5 => Nombres.Cinq,
+				6 => Nombres.Six,
+				7 => Nombres.Sept,
+				8 => Nombres.Huit,
+				9 => Nombres.Neuf,
+			};
 
 			Assert.Equal(expected, result);
 		}
